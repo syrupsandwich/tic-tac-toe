03c9a5882e006b9b1bbc934989c48eaf2831fb4c
sau <dontsendsamspam@gmail.com>
2022-11-03 18:49:07 -0700

    Cause click to mark the gameboard

--- program/tic-tac-toe.js
+++ program/tic-tac-toe.js
@@ -1,8 +1,24 @@
-(function (){
-  let grid = document.querySelector('.grid');
-  for(let i = 0; i < 9; i++){
-    let cell = document.createElement('div');
-    cell.id = i;
-    grid.appendChild(cell);
-  }
+const gameboard = (() => {
+  const grid = document.querySelector('.grid');
+
+  (function (){
+    for(let i = 0; i < 9; i++){
+      let cell = document.createElement('div');
+      cell.id = i;
+      grid.appendChild(cell);
+    }
+  })();
+
+  const cells = Array.from(document.querySelector('.grid').childNodes);
+  const markCell = (id, marker) => {
+    cells[id].textContent = marker;
+  };
+  
+  return { cells, grid, markCell };
 })();
+
+gameboard.grid.addEventListener('click', (e) => {
+  if (e.target.parentNode !== gameboard.grid) { return };
+  if (e.target.textContent !== '') { return };
+  gameboard.markCell(e.target.id, 'x');
+});
