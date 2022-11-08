80d3433b03dae420e2aee20a92a7c45c45422fa3
sau <dontsendsamspam@gmail.com>
2022-11-07 17:08:36 -0800

    Add an option to change grid dimensions
    It requires the grid be generated in a different way. Font size will
    change automatically.

--- program/tic-tac-toe.js
+++ program/tic-tac-toe.js
@@ -1,38 +1,78 @@
 const gameboard = (() => {
   const grid = document.querySelector('.grid');
+  const maxCells = 81;
+  const allCells = [];
+  for (let i = 0; i < maxCells; i ++){
+    allCells.push(document.createElement('div'));
+  }
 
-  (function (){
-    for(let i = 0; i < 9; i++){
-      let cell = document.createElement('div');
-      cell.id = i;
-      grid.appendChild(cell);
+  const resetGrid = (dimension = 3) => {
+    grid.style.setProperty('--grid-dimension', dimension);
+
+    if(grid.childElementCount > 0){
+      allCells.forEach(element => { 
+        element.textContent = '';
+        element.id = '';
+        element.removeAttribute('class');
+        element.remove();
+      });
     }
-  })();
+    
+    requiredCells = dimension * dimension;
+    for(let i = 0; i < requiredCells; ++i){
+      allCells[i].id = i;
+      grid.appendChild(allCells[i]);
+    }
 
-  const cells = Array.from(document.querySelector('.grid').childNodes);
+  };
+  resetGrid();
+
   const markCell = (id, marker) => {
-    cells[id].textContent = marker;
+    allCells[id].textContent = marker;
   };
   
   const endTurn = () => {
     if (player2.automated){
     let options = [];
-    cells.forEach((cell)=>{ if (cell.textContent === '') { options.push(cell.id) } });
+    allCells.forEach((cell)=>{
+      if (cell.textContent === '' && cell.id !== '') {
+        options.push(cell.id) }
+      });
     player2.move(options);
     }
   }
 
-  return { cells, grid, markCell, endTurn};
+  const rangeInput = document.querySelector('input');
+  const output = document.querySelector('output');
+  const resetBtn = document.querySelector('.reset');
+
+  resetBtn.addEventListener('click', function() {
+    resetGrid(output.value.charAt(0));
+  });
+
+  function setDefaultState() {
+    output.value = `${rangeInput.value} * ${rangeInput.value}`;
+  };
+  
+  rangeInput.addEventListener('input', function() {
+    output.value = `${this.value} x ${this.value}`;
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    setDefaultState();
+  });
+
+  return { grid, markCell, endTurn};
 })();
 
-gameboard.grid.addEventListener('click', (e) => {
-  if (e.target.parentNode !== gameboard.grid) { return };
-  if (e.target.textContent !== '') { return };
-  gameboard.markCell(e.target.id, player1.marker);
-  gameboard.endTurn();
-});
-
 const player = (mark, isBot = false) => {
+  gameboard.grid.addEventListener('click', (e) => {
+    if (e.target.parentNode !== gameboard.grid) { return };
+    if (e.target.textContent !== '') { return };
+    gameboard.markCell(e.target.id, player1.marker);
+    gameboard.endTurn();
+  });
+
   let marker = mark.charAt(0);
   let score = 0;
 
@@ -41,7 +81,8 @@ const player = (mark, isBot = false) => {
     const getRandomInt = (max) => { return Math.floor(Math.random() * max) };
     const move = (options) => {
       if (options.length === 0){ return };
-      gameboard.markCell(options[getRandomInt(options.length)], player2.marker);
+      let randomId = getRandomInt(options.length);
+      gameboard.markCell(options[randomId], player2.marker);
     }
     return { marker, score, move, automated};
   }
